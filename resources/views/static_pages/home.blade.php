47e9d15d77b877c6b6df288aed1c25d7406403bb
renren <279505874@qq.com>
2024-09-02 07:17:59 +0000

    初始化样式,sass集成

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.default')
 @section('title','主页')
 @section('content')
-<h1>主页</h1>
+<div class="jumbotron">
+<h1>Hello Laravel</h1>
+<p class="lead">你现在所看到的是 <a href="www.baidu.com">百度百度</a></p>
+<p>一切，将从这里开始。</p>
+<button type="button" class="btn btn-success">现在注册</button>
+</div>
 @stop
