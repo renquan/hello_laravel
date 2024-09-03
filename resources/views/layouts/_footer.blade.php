edc5fb2811b2b7a30751c0acdec6ab2a86df2f4a
renren <279505874@qq.com>
2024-09-03 07:35:17 +0000

    4.6 命名路由

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <img class="brand-icon" src="https://git-scm.com/images/logo@2x.png" width="100"/>
+    <a href="https://laravel-china.org/courses" target=_blank>
+    刻意练习，每日精进
+    </a>
+
+    <div class="float-end">
+        <a href="{{ route('about') }}" >关于</a>
+    </div>
+</footer>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -4,7 +4,7 @@
 <div class="jumbotron">
 <h1>Hello Laravel</h1>
 <p class="lead">你现在所看到的是 <a href="www.baidu.com">百度百度</a></p>
-<p>一切，将从这里开始。</p>
+<p>一切，将从这里开始1。</p>
 <button type="button" class="btn btn-success">现在注册</button>
 </div>
 @stop
