ca1213eadf7c61ad04647a7f41b2d0317faf9430
renren <279505874@qq.com>
2024-09-03 08:30:11 +0000

    6.6 用户注册成功

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,15 +8,31 @@ use Illuminate\Http\Request;
 class UsersController extends Controller
 {
     //用户注册页面
-    public function create() {
+    public function create()
+    {
         return view('users.create');
     }
 
     //用户信息展示
-    public function show(User $user) {
-        return view('users.show',compact('user'));
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
     }
 
-
-
+    //验证并保存用户
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
+    }
 }
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,17 +1,21 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>@yield('title','weibo APP')</title>
-@vite(['resources/css/app.css', 'resources/js/app.js','resources/sass/app.scss'])
+    <title>@yield('title','weibo APP')</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js','resources/sass/app.scss'])
 </head>
+
 <body>
     @include('layouts._header')
 
     <div class="container">
         <div class="offset-md-1 col-md-10">
+            @include('shared._messages')
             @yield('content')
             @include('layouts._footer')
         </div>
     </div>
 </body>
+
 </html>
