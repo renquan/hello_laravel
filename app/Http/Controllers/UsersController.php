2b2c6ac6286aec837ffe3ffa87f47c9ee55f707a
任一一 <279505874@qq.com>
2024-09-04 11:31:18 +0800

    退出登录

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -37,4 +37,11 @@ class UsersController extends Controller
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
     }
+
+    public function destroy(){
+        Auth::logout();
+        session()->flash('success','退出登录');
+
+    }
+
 }
